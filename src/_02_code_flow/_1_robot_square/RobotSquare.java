97c1fa797dee424f4161113f383398e47a6edf80
league <league@192.168.1.135>
2024-02-08 20:06:44 -0800

    ...

--- src/_02_code_flow/_1_robot_square/RobotSquare.java
+++ src/_02_code_flow/_1_robot_square/RobotSquare.java
@@ -17,27 +17,32 @@ public class RobotSquare {
 
         // PEN.  Put the robot's pen down so it can draw, Use this command:
 		 /**     rob.penDown();    **/
+    	rob.penDown();
 
         // SPEED. Make the robot move quickly. Use this command:
 		  /**     rob.setSpeed(<speed>);    **/
- 
+    	rob.setSpeed(100);
+    	
     	// COUNT. Create an int variable that will count how many sides of the square we have drawn.
     	//        The start value will be zero because no sides have yet been drawn. Use this code:
 		  /**     int count = 0;    **/
+    	int n = 22;
     	
-
-        // DRAW.  Draw a line. Use this command:
-    		  /**     rob.move(<distance>);    **/
-  
-
-	// TURN.  Turn 90 degrees. Use this command:
-    		  /**     rob.turn(<angle>);    **/
-
+    	for(int count = 0; count < n; count +=1) {
     	
-    	// INCREASE COUNT. Add one to the number of sides that have now been drawn like this:
-    		  /**     count += 1;    **/
-
+	        // DRAW.  Draw a line. Use this command:
+	    		  /**     rob.move(<distance>);    **/
+	    	rob.move(10);
+	
+		    // TURN.  Turn 90 degrees. Use this command:
+	    		  /**     rob.turn(<angle>);    **/
+	    	rob.turn(360/n);
     	
+	    	// INCREASE COUNT. Add one to the number of sides that have now been drawn like this:
+    		 /**     count += 1;    **/
+    	}
+    	
+   
     	// Run the program. Did the Robot draw a line and turn?
     	 
     	
